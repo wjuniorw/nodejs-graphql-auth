cc349868219c9b03c1b5c6f58fb82d82ccc69a35
wjuniorw <wilson.junior90@gmail.com>
2018-11-15 00:44:26 -0300

    return static data on mutations

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -5,6 +5,7 @@ export default {
     users: (parent, args, context) => ([{name: 'teste', email: 'test@mail.com'}]),
   },
   Mutation: {
-    //
+    login: async(parent, args, context)=> ({ ok: true, token: 'hlsfsd454', user: {email: '@'}}),
+    register: async(parent, args, context)=> ({ ok: true, token: 'hlsfsd454', user: {email: '@'}})
   }
 }
